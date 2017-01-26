7c3541b6db911390c16fd9cb48557408085f6084
Szczepan Zalega <szczepan@nitrokey.com>
2017-01-26 11:37:29 +0100

    ThreadWorker (3)
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/core/ThreadWorker.h
+++ src/core/ThreadWorker.h
@@ -8,46 +8,59 @@
 #include <QVariant>
 #include <QMap>
 #include <functional>
+#include <QMutexLocker>
 
 namespace ThreadWorkerNS {
   using Data = QMap<QString, QVariant>;
   class Worker : public QObject
   {
-  Q_OBJECT
-  public:
-    QMutex mutex;
-  public slots:
-    void fetch_data() {
-      auto data = datafunc();
-      emit finished(data);
-    }
+    Q_DISABLE_COPY(Worker)
+    Q_OBJECT
+    public:
+    Worker(QObject *parent, const std::function<Data()> &datafunc) :
+        QObject(parent),
+        datafunc(datafunc) {}
 
-  signals:
-    void finished(Data data);
-  private:
-    std::function<Data()> datafunc;
-  };
+  public slots:
+      void fetch_data() {
+        QMutexLocker lock(&mutex);
+        auto data = datafunc();
+        emit finished(data);
+      }
+
+    signals:
+      void finished(Data data);
+    private:
+      QMutex mutex;
+      std::function<Data()> datafunc;
+    };
 }
 
 
 class ThreadWorker : public QObject {
-using namespace ThreadWorkerNS;
+using Worker = ThreadWorkerNS::Worker;
+using Data = ThreadWorkerNS::Data;
 Q_OBJECT
 Q_DISABLE_COPY(ThreadWorker)
 
-
 public:
-  ThreadWorker(){
+
+  ThreadWorker(const std::function<Data()> &datafunc,
+               const std::function<void(Data)> &usefunc, QObject *parent=nullptr) :
+      QObject(parent),
+        worker(this, datafunc),
+        usefunc(usefunc){
     connect(&worker_thread, SIGNAL(started()), &worker, SLOT(fetch_data()));
-    connect(&worker, SIGNAL(finished(bool)), this, SLOT(use_data(Data)));
+    connect(&worker, SIGNAL(finished(Data)), this, SLOT(use_data(Data)));
     worker.moveToThread(&worker_thread);
     worker_thread.start();
   }
+
   ~ThreadWorker(){
     stop_thread();
   }
   void use_data(Data data){
-
+    usefunc(data);
   };
 
   void stop_thread(){
@@ -57,7 +70,7 @@ public:
 private:
   Worker worker;
   QThread worker_thread;
-
+  std::function<void(Data)> usefunc;
 };
 
 
