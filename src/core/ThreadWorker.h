8d17a4d12ab02f38802767deb915fd67af0d8cdb
Szczepan Zalega <szczepan@nitrokey.com>
2017-01-26 18:22:52 +0100

    ThreadWorker (9) split declaration and definition
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/core/ThreadWorker.h
+++ src/core/ThreadWorker.h
@@ -2,92 +2,62 @@
 #define NITROKEYAPP_THREADWORKER_H
 
 #include <QObject>
-#include <QMutex>
+#include <functional>
 #include <QThread>
 #include <QString>
+#include <QMutex>
 #include <QVariant>
 #include <QMap>
-#include <functional>
-#include <QMutexLocker>
-#include <QDebug>
 
-
-//typedef QMap<QString, QVariant> Data;
 using Data = QMap<QString, QVariant>;
-//Q_DECLARE_METATYPE(Data)
 
 namespace ThreadWorkerNS {
-  class Worker : public QObject
-  {
-    Q_OBJECT
-    Q_DISABLE_COPY(Worker)
-    public:
-    Worker(QObject *parent, const std::function<Data()> &datafunc) :
-        QObject(parent),
-        datafunc(datafunc) {}
+  class Worker : public QObject {
+  Q_OBJECT
+  Q_DISABLE_COPY(Worker)
+
+  public:
+    Worker(QObject *parent, const std::function<Data()> &datafunc);
 
   public slots:
-      void fetch_data() {
-        QMutexLocker lock(&mutex);
-        auto data = datafunc();
-        emit finished();
-        emit finished(data);
-      }
+    void fetch_data();
 
-    signals:
-      void finished(QMap<QString, QVariant> data);
-      void finished();
-    private:
-      QMutex mutex;
-      std::function<Data()> datafunc;
-    };
+  signals:
+    void finished(QMap<QString, QVariant> data);
+    void finished();
 
+  private:
+    QMutex mutex;
+    std::function<Data()> datafunc;
+  };
 }
 
+/***
+ * Allocate on heap and add parent (to delete on its destruction)
+ */
 class ThreadWorker : public QObject {
-using Worker = ThreadWorkerNS::Worker;
-Q_OBJECT
-Q_DISABLE_COPY(ThreadWorker)
+  using Worker = ThreadWorkerNS::Worker;
+  Q_OBJECT
+  Q_DISABLE_COPY(ThreadWorker)
 
 public:
-
   ThreadWorker(const std::function<Data()> &datafunc,
-               const std::function<void(Data)> &usefunc, QObject *parent=nullptr) :
-      QObject(parent),
-        worker(nullptr, datafunc),
-        usefunc(usefunc){
+               const std::function<void(Data)> &usefunc,
+               QObject *parent = nullptr);
 
-    connect(&worker, SIGNAL(finished()), this, SLOT(worker_finished()), Qt::QueuedConnection);
-    connect(&worker_thread, SIGNAL(started()), &worker, SLOT(fetch_data()), Qt::QueuedConnection);
-    connect(&worker, SIGNAL(finished(QMap<QString, QVariant>) ),
-            this, SLOT( use_data(QMap<QString, QVariant>) ), Qt::QueuedConnection);
-    worker.moveToThread(&worker_thread);
-    worker_thread.start();
-  }
+  ~ThreadWorker();
 
-  ~ThreadWorker(){
-    stop_thread();
-  }
-
-  void stop_thread(){
-    worker_thread.quit();
-    worker_thread.wait();
-  }
-
-  private slots:
-  void worker_finished(){
-    qDebug() << "worker finished";
-  };
-  void use_data(QMap<QString, QVariant> data){
-    QMutexLocker lock(&mutex);
-    usefunc(data);
-  };
+private slots:
+  void worker_finished();
+  void use_data(QMap<QString, QVariant> data);
 
 private:
   Worker worker;
   QThread worker_thread;
   std::function<void(Data)> usefunc;
   QMutex mutex;
+
+  void stop_thread();
 };
 
 
