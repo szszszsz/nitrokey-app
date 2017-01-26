3bfe61970735bdbd185c3ecdbd4a6d339fd5c630
Szczepan Zalega <szczepan@nitrokey.com>
2017-01-26 11:25:16 +0100

    ThreadWorker (2)
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/core/ThreadWorker.h
+++ src/core/ThreadWorker.h
@@ -4,32 +4,58 @@
 #include <QObject>
 #include <QMutex>
 #include <QThread>
+#include <QString>
+#include <QVariant>
+#include <QMap>
+#include <functional>
 
 namespace ThreadWorkerNS {
+  using Data = QMap<QString, QVariant>;
   class Worker : public QObject
   {
   Q_OBJECT
   public:
     QMutex mutex;
-//    struct {
-//      } data;
   public slots:
-    void fetch_data();
+    void fetch_data() {
+      auto data = datafunc();
+      emit finished(data);
+    }
+
   signals:
-    void finished(bool connected);
+    void finished(Data data);
+  private:
+    std::function<Data()> datafunc;
   };
 }
 
 
 class ThreadWorker : public QObject {
+using namespace ThreadWorkerNS;
 Q_OBJECT
 Q_DISABLE_COPY(ThreadWorker)
 
 
 public:
+  ThreadWorker(){
+    connect(&worker_thread, SIGNAL(started()), &worker, SLOT(fetch_data()));
+    connect(&worker, SIGNAL(finished(bool)), this, SLOT(use_data(Data)));
+    worker.moveToThread(&worker_thread);
+    worker_thread.start();
+  }
+  ~ThreadWorker(){
+    stop_thread();
+  }
+  void use_data(Data data){
 
+  };
+
+  void stop_thread(){
+    worker_thread.quit();
+    worker_thread.wait();
+  }
 private:
-  ThreadWorkerNS::Worker worker;
+  Worker worker;
   QThread worker_thread;
 
 };
